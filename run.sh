82612d4f20cea93b0e711bf80246c3e04b121621
rafimahrus <mahrusrafi@gmail.com>
2026-07-20 16:28:57 +0700

    add script restart systemctl (make sure service restarted to newest version)

--- run.sh
+++ run.sh
@@ -13,3 +13,12 @@ go build cmd/main.go
 
 echo "=== Running application ==="
 ./main
+
+echo "=== Restarting systemctl service ==="
+systemctl daemon-reload
+
+echo "=== Restarting service ==="
+systemctl restart povai-idn.service
+
+echo "=== Checking service status ==="
+systemctl status povai-idn.service
